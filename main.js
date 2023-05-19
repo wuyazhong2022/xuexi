fac0d40f4558d68eb2ed971d16b94a30fc80f036
wuyazhong2022 <101036230+wuyazhong2022@users.noreply.github.com>
2023-05-19 20:39:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ console.show()
 
    打开浏览器进去网站()
 
-    // launchApp("浏览器");
+  
     sleep(2000);
+
     var isLogin = false;
 
     while (!isLogin) {
@@ -42,7 +43,7 @@ console.show()
             }
         }
         sleep(1000);
-        toastLog("已复制网址请点击右上角空白处输入框粘贴网址，如遇遮挡可以点击悬浮窗口窗口--最小化，完成输入登录后在右上角按钮打开窗口");
+        console.log("已复制网址请点击右上角空白处输入框粘贴网址，如遇遮挡可以点击悬浮窗口窗口--最小化，完成输入登录后在右上角按钮打开窗口");
         //setText("https://ptce.gx12333.net/")
 
         sleep(1000);
@@ -56,162 +57,144 @@ console.show()
 
 
 
-    function 打开浏览器进去网站() {
-        launchApp("浏览器");
-        setClip("https://ptce.gx12333.net/")
+function 打开浏览器进去网站() {
+    launchApp("浏览器");
+    setClip("https://ptce.gx12333.net/")
 
-        sleep(2000);
-        toastLog("已复制网址请点击右上角空白处输入框粘贴网址，如遇遮挡可以点击悬浮窗口窗口--最小化，完成输入登录后在右上角按钮打开窗口");
+    sleep(2000);
+    console.log("已复制网址请点击右上角空白处输入框粘贴网址，如遇遮挡可以点击悬浮窗口窗口--最小化，完成输入登录后在右上角按钮打开窗口");
 
-        sleep(2000);
-                toastLog("已复制网址请点击右上角空白处输入框粘贴网址，如遇遮挡可以点击悬浮窗口窗口--最小化，完成输入登录后在右上角按钮打开窗口");
+    sleep(2000);
+    console.log("已复制网址请点击右上角空白处输入框粘贴网址，如遇遮挡可以点击悬浮窗口窗口--最小化，完成输入登录后在右上角按钮打开窗口");
 
-        sleep(2000);
-        toastLog("已复制网址请点击右上角空白处输入框粘贴网址，如遇遮挡可以点击悬浮窗口窗口--最小化，完成输入登录后在右上角按钮打开窗口");
+    sleep(1000);
+    //等待页面加载完成，时间可以根据网速调整
 
-        // setClip("https://ptce.gx12333.net/")
-        sleep(1000);
-        //等待页面加载完成，时间可以根据网速调整
-
-        toastLog("等待页面加载完成");
+    console.log("等待页面加载完成");
 
 
 
-    }
+}
 
 
 
+function 循环看视频() {
 
-
-
-    function 循环看视频() {
-
-        sleep(2000);
-        while (true) {
-            // 遍历描述控件，找到开始学习按钮并点击 
-            var startButton = desc("开始学习").findOne(1000);
-            if (startButton) {
-                toastLog(startButton.click());
-                toastLog("开始学习")
-                //showTime()
-                // 等待10秒 
-                toastLog("等待播放完成不要离开当前页面");
-
-                // 调用函数
-                replayAndExit();
-
-            } else {
-                // 如果没有找到开始学习按钮，尝试找到继续学习按钮 
-
-                var continueButton = desc("继续学习").findOne(1000);
-
-                if (continueButton) {
-
-                    log(continueButton.click());
-                    toastLog("继续学习")
-                    showTime1()
-                    // 等待15秒 
-                    toastLog("等待播放完成不要离开当前页面")
-
-                    // 调用函数 出现循环播放控件，完成播放
-                    replayAndExit();
-                } else {
-                    // 如果都没有找到，退出循环
-                    sleep(2000);
-                    toastLog("学习视频已完成，退出循环，学习结束")
-                    sleep(2000);
-                    break;
-                }
-
-                sleep(1000);
-                toastLog("学习完1 ");
-
-            }
-            toastLog("返回并继续点击开始学习或者找继续学习 ");
-
-            //back();
+    sleep(2000);
+    while (true) {
+        // 遍历描述控件，找到开始学习按钮并点击 
+        var startButton = desc("开始学习").findOne(1000);
+        if (startButton) {
+            console.log(startButton.click());
+            console.log("开始学习")
             sleep(2000);
-            // 等待1秒
-            className("android.view.View").desc("确定").findOne().click()
-            toastLog("点击确定 ");
+            console.log("等待播放完成不要离开当前页面");
+            sleep(2000);
+            // 调用函数
+            replayAndExit();
 
-            sleep(3000);
-        }
-        sleep(1000);
-        toastLog("学习完成 ");
-// 循环结束后关闭控制台
+        } else {
+            // 如果没有找到开始学习按钮，尝试找到继续学习按钮 
 
-console.hide();
+            var continueButton = desc("继续学习").findOne(1000);
 
-// 退出脚本
+            if (continueButton) {
 
-exit();
-    }
+                log(continueButton.click());
+                console.log("继续学习")
+                sleep(1000);
+                // 等待15秒 
+                console.log("等待播放完成不要离开当前页面")
 
-
-
-    // 显示5-7分钟随机倒计时气泡
-    // // 随机生成5-7分钟 
-    function showTime() {
-        var showTime = Math.random() * (1) * 60 * 1000 + 1 * 60 * 1000;
-        var time = new Date(Date.now() + showTime);
-        while (Date.now() < time) {
-            // 屏幕显示倒计时气泡 
-            console.log(Math.round((time - Date.now()) / 1000) + 's');
-            sleep(1000)
-            toast(Math.round((time - Date.now()) / 1000) + 's');
-            //  sleep(1000);
-
-        }
-    }
-
-
-
-
-    // 定义函数
-    function replayAndExit() {
-        // 初始化计数器 
-        var count = 0;
-        // 循环找到text为"Replay"的控件
-        while (true) {
-
-
-            var replayBtn = text("Replay").findOne();
-            if (replayBtn != null) {
-                //// 找到控件后输出计数器的值并退出循环 
-                console.log("找到播放控件，共循环找控件 " + count + " 次");
-
-                showTime()
-                // 点击退出课程按钮
-                var exitBtn = className("android.widget.Button").text("退出课程").findOne();
-                exitBtn.click();
+                // 调用函数 出现循环播放控件，完成播放
+                replayAndExit();
+            } else {
+                // 如果都没有找到，退出循环
                 sleep(2000);
-                // 点击确定按钮
-                var confirmBtn = className("android.widget.Button").text("确定").findOne();
-                confirmBtn.click();
-                // 执行完毕后退出函数
-                return;
+                console.log("学习视频已完成，退出循环，学习结束")
+                sleep(2000);
+                break;
             }
-            // 如果没有找到，则等待1秒后重新找
+
             sleep(1000);
-            // 计数器自增 
-            count++;
+            console.log("没有开始学习了，开始找继续学习 ");
+
+        }
+        console.log("返回到视频列表页面 ");
+
+        //back();
+        sleep(2000);
+        // 等待1秒
+        className("android.view.View").desc("确定").findOne().click()
+        console.log("点击确定 ");
+
+        sleep(3000);
+    }
+    sleep(1000);
+    console.log("学习完成 ");
+    // 循环结束后关闭控制台
+
+    console.hide();
+
+    // 退出脚本
+
+    exit();
+}
+
+
+
+// 显示30-50秒随机倒计时气泡
+
+
+function showTime() {
+
+    var showTime = Math.random() * (1) * 60 * 1000 + 30 * 1000;
+
+    var time = new Date(Date.now() + showTime);
+
+    while (Date.now() < time) {
+
+        // 屏幕显示倒计时气泡
+
+        console.log(Math.round((time - Date.now()) / 1000) + 's');
+
+        sleep(1000)
+
+    }
+
+}
+
+
+
+// 定义函数// 循环找到text为"Replay"的控件，找到了说明播放完成
+function replayAndExit() {
+    // 初始化计数器 
+    var count = 0;
+    // 循环找到text为"Replay"的控件
+    while (true) {
+
+
+        var replayBtn = text("Replay").findOne();
+        if (replayBtn != null) {
+            //// 找到控件后输出计数器的值并退出循环 
             console.log("找到播放控件，共循环找控件 " + count + " 次");
+
+            showTime()
+            // 点击退出课程按钮
+            var exitBtn = className("android.widget.Button").text("退出课程").findOne();
+            exitBtn.click();
+            sleep(2000);
+            // 点击确定按钮
+            var confirmBtn = className("android.widget.Button").text("确定").findOne();
+            confirmBtn.click();
+            // 执行完毕后退出函数
+            return;
         }
+        // 如果没有找到，则等待1秒后重新找
+        sleep(1000);
+        // 计数器自增 
+        count++;
+        console.log("找到播放控件，共循环找控件 " + count + " 次");
     }
-
-
-    // 随机生成5-7分钟 
-    function showTime1() {
-        var showTime1 = Math.random() * (1) * 60 * 1000 + 7 * 60 * 1000;
-        var time = new Date(Date.now() + showTime1);
-        while (Date.now() < time) {
-            // 屏幕显示倒计时气泡 
-            console.log(Math.round((time - Date.now()) / 1000) + 's');
-            sleep(1000)
-            toast(Math.round((time - Date.now()) / 1000) + 's');
-            //  sleep(1000);
-
-        }
-    }
+}
 
